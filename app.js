2b9605624424f47f814aadf153cde1860fc977e8
user1 <ayari154ibtihel@gmail.com>
2024-03-02 12:03:36 +0100

    third commit

--- app.js
+++ app.js
@@ -2,15 +2,17 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = express();
-
+//const user = require("./routes/auth.js");
+//const post  = require("./routes/posts.js") ;
 dotenv.config();
+app.use(express.json()) ;
 
-const MONGODB_URI = process.env.MONGODB_URL;
+const MONGODB_URI = process.env.MONGODB_URL
 const PORT = process.env.PORT || 3000;
 
 // Define a route
-app.get('/', (req, res) => {});
-
+//app.use("/auth",user) ;
+//app.use("/posts",post) ;
 // Connection to MongoDB and start server
 mongoose.connect(MONGODB_URI)
     .then(() => {
--- routes/posts.js
+++ routes/posts.js
@@ -69,22 +69,6 @@ app.delete('/posts/:id', getPost, async (req, res) => {
   }
 });
 
-// Middleware function to get a single post by ID
-async function getPost(req, res, next) {
-  let post;
-  try {
-    post = await Post.findById(req.params.id);
-    if (post == null) {
-      return res.status(404).json({ message: 'Cannot find post' });
-    }
-  } catch (err) {
-    return res.status(500).json({ message: err.message });
-  }
-
-  res.post = post;
-  next();
-}
-
 app.listen(3000, () => console.log('Server started...'));
 
 
